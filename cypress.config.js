dd4440ae5727ace200ed7edfdf42753451efc090
James Houghton <james.p.houghton@gmail.com>
2022-10-05 16:23:36 -0400

    github action for cypress test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
+  projectId: "z7p66s",
   component: {
     devServer: {
       framework: "react",
