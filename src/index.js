d2dfe5dc4dc550c13b0ebd02e11172919f4bb0de
James Houghton <james.p.houghton@gmail.com>
2022-10-13 22:21:52 -0400

    Add demographics survey
    Still needs updating, localization to non-US countries

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import SurveyFactory from "./surveyFactory.jsx"
 const ExampleSurvey = await SurveyFactory('exampleSurvey');
 const TeamViability = await SurveyFactory('teamViability');
 const DiscussionQualityControl = await SurveyFactory('discussionQualityControl');
+const Demographics = await SurveyFactory('demographics');
 
 export { 
     ExampleSurvey,
     TeamViability, 
-    DiscussionQualityControl 
+    DiscussionQualityControl,
+    Demographics
 }
