85847c72b8410f845c6386f224276117b99b4c63
James Houghton <james.p.houghton@gmail.com>
2022-10-20 16:06:34 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,19 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
   projectId: "z7p66s",
+
   component: {
     devServer: {
       framework: "react",
       bundler: "vite",
+    },
+    specPattern: "./surveys/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "./cypress/support/component.js",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
