69c3a80ed22ce357ca794bdaea4d7f49d9c02a85
James Houghton <james.p.houghton@gmail.com>
2023-01-27 20:38:31 -0500

    Update politicalPartyUS.cy.jsx

--- surveys/politicalPartyUS/politicalPartyUS.cy.jsx
+++ surveys/politicalPartyUS/politicalPartyUS.cy.jsx
@@ -10,55 +10,55 @@ describe("PoliticaPartyUS", () => {
     cy.spy(dummy, "set").as("callback");
     cy.mount(<PoliticalPartyUS onComplete={dummy.set} />);
 
-    //   cy.get('[data-name="party"] input[value="Republican"]').click({
-    //     force: true,
-    //   });
+    cy.get('[data-name="party"] input[value="Republican"]').click({
+      force: true,
+    });
 
-    //   cy.get(
-    //     '[data-name="republicanStrength"] input[value="Strong Republican"]'
-    //   ).click({ force: true });
+    cy.get(
+      '[data-name="republicanStrength"] input[value="Strong Republican"]'
+    ).click({ force: true });
 
-    //   cy.screenshot("politicalPartyUS/page1", { overwrite: true });
+    cy.screenshot("politicalPartyUS/page1", { overwrite: true });
 
-    //   cy.get(`input[type="button"][value="Next"]`).click({ force: true });
+    cy.get(`input[type="button"][value="Next"]`).click({ force: true });
 
-    //   cy.screenshot("politicalPartyUS/page2", { overwrite: true });
+    cy.screenshot("politicalPartyUS/page2", { overwrite: true });
 
-    //   cy.get(`input[type="button"][value="Complete"]`).click({ force: true });
+    cy.get(`input[type="button"][value="Complete"]`).click({ force: true });
 
-    //   cy.get("@callback").should("have.been.called");
-    //   cy.get("@callback").then((spy) => {
-    //     const spyCall = spy.getCall(-1).args[0];
-    //     console.log(spyCall);
-    //     expect(spyCall["result"]["party"]).to.eq("Republican");
-    //     expect(spyCall["result"]["normPosition"]).to.eq(1);
-    //     expect(spyCall["result"]["normImportance"]).to.eq(0.5);
-    //   });
+    cy.get("@callback").should("have.been.called");
+    cy.get("@callback").then((spy) => {
+      const spyCall = spy.getCall(-1).args[0];
+      console.log(spyCall);
+      expect(spyCall["result"]["party"]).to.eq("Republican");
+      expect(spyCall["result"]["normPosition"]).to.eq(1);
+      expect(spyCall["result"]["normImportance"]).to.eq(0.5);
+    });
   });
 
-  // it("completesOther", () => {
-  //   cy.spy(dummy, "set").as("callback");
-  //   cy.mount(<PoliticalPartyUS onComplete={dummy.set} />);
+  it("completesOther", () => {
+    cy.spy(dummy, "set").as("callback");
+    cy.mount(<PoliticalPartyUS onComplete={dummy.set} />);
 
-  //   cy.get('[data-name="party"] input[value="Other"]').click({
-  //     force: true,
-  //   });
+    cy.get('[data-name="party"] input[value="Other"]').click({
+      force: true,
+    });
 
-  //   cy.get('[data-name="partyOther"] input').click().type("Fancy dress party");
+    cy.get('[data-name="partyOther"] input').click().type("Fancy dress party");
 
-  //   cy.screenshot("politicalPartyUS/page1_other", { overwrite: true });
+    cy.screenshot("politicalPartyUS/page1_other", { overwrite: true });
 
-  //   cy.get(`input[type="button"][value="Next"]`).click({ force: true });
+    cy.get(`input[type="button"][value="Next"]`).click({ force: true });
 
-  //   cy.get(`input[type="button"][value="Complete"]`).click({ force: true });
+    cy.get(`input[type="button"][value="Complete"]`).click({ force: true });
 
-  //   cy.get("@callback").should("have.been.called");
-  //   cy.get("@callback").then((spy) => {
-  //     const spyCall = spy.getCall(-1).args[0];
-  //     console.log(spyCall);
-  //     expect(spyCall["result"]["party"]).to.eq("Other");
-  //     expect(spyCall["result"]["normPosition"]).to.be.undefined;
-  //     expect(spyCall["result"]["normImportance"]).to.eq(0.5);
-  //   });
-  // });
+    cy.get("@callback").should("have.been.called");
+    cy.get("@callback").then((spy) => {
+      const spyCall = spy.getCall(-1).args[0];
+      console.log(spyCall);
+      expect(spyCall["result"]["party"]).to.eq("Other");
+      expect(spyCall["result"]["normPosition"]).to.be.undefined;
+      expect(spyCall["result"]["normImportance"]).to.eq(0.5);
+    });
+  });
 });
