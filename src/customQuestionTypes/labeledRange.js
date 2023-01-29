69e8f85d9475307713274b1b0346c038dd1d492f
James Houghton <james.p.houghton@gmail.com>
2023-01-29 09:44:09 -0500

    update version

--- src/customQuestionTypes/labeledRange.js
+++ src/customQuestionTypes/labeledRange.js
@@ -3,24 +3,26 @@
 Modifies the existing range component to add track labels.
  the updated syntax for a range question should be:
 
-{
-  "type": "text",
-  "inputType": "range",
-  "name": "republicanImportance",
-  "title": "How important is being a Republican to you?",
-  "description": "Below is a range from 0 to 100 indicating how important this is to you. Click on any space within this range and a bar will appear. Feel free to move that bar around to the number that best represents your answer.",
-  "visibleIf": "{party} = 'Republican'",
-  "min": 0,
-  "max": 100,
-  "step": 1,
-  "valueLabels": [
-    { "value": 0, "text": "Not important at all" },
-    { "value": 25, "text": "" },
-    { "value": 50, "text": "Moderately Important" },
-    { "value": 75, "text": "" },
-    { "value": 100, "text": "Extremely Important" }
-  ]
-},
+ ```
+  {
+    "type": "text",
+    "inputType": "range",
+    "name": "republicanImportance",
+    "title": "How important is being a Republican to you?",
+    "description": "Below is a range from 0 to 100 indicating how important this is to you. Click on any space within this range and a bar will appear. Feel free to move that bar around to the number that best represents your answer.",
+    "visibleIf": "{party} = 'Republican'",
+    "min": 0,
+    "max": 100,
+    "step": 1,
+    "valueLabels": [
+      { "value": 0, "text": "Not important at all" },
+      { "value": 25, "text": "" },
+      { "value": 50, "text": "Moderately Important" },
+      { "value": 75, "text": "" },
+      { "value": 100, "text": "Extremely Important" }
+    ]
+  },
+```
 
 */
 
