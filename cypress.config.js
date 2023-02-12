13358bdb66858bc68870d54facdd030c9fbf2cab
amirrahman132132 <52195981+amirrahman132132@users.noreply.github.com>
2023-02-12 09:54:04 +0600

    restyled components with css and javascript changes for labeledrange slider

--- cypress.config.js
+++ cypress.config.js
@@ -37,4 +37,10 @@ module.exports = defineConfig({
       });
     },
   },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
--- src/customQuestionTypes/labeledRange.js
+++ src/customQuestionTypes/labeledRange.js
@@ -27,70 +27,88 @@ Modifies the existing range component to add track labels.
 */
 
 export function labeledRange(SurveyJS) {
-  var widget = {
-    name: "labeledRange",
+    var widget = {
+        name: "labeledRange",
 
-    isFit: (question) => {
-      return question.getType() === "text" && question.inputType === "range";
-    },
+        isFit: (question) => {
+            return question.getType() === "text" && question.inputType === "range";
+        },
 
-    isDefaultRender: true,
+        isDefaultRender: true,
 
-    init() {
-      SurveyJS.JsonObject.metaData.addProperty("text", {
-        name: "valueLabels",
-        type: "itemvalues",
-        category: "slider",
-        default: [
-          { value: 0, text: "Strongly Disagree" },
-          { value: 25, text: "" },
-          { value: 50, text: "Neither agree nor disagree" },
-          { value: 75, text: "" },
-          { value: 100, text: "Strongly Agree" },
-        ],
-      });
+        init() {
+            SurveyJS.JsonObject.metaData.addProperty("text", {
+                name: "valueLabels",
+                type: "itemvalues",
+                category: "slider",
+                default: [
+                    { value: 0, text: "Strongly Disagree" },
+                    { value: 25, text: "" },
+                    { value: 50, text: "Neither agree nor disagree" },
+                    { value: 75, text: "" },
+                    { value: 100, text: "Strongly Agree" },
+                ],
+            });
 
-      SurveyJS.JsonObject.metaData.addProperty("text", {
-        name: "progressBar",
-        type: "boolean",
-        category: "slider",
-        default: false,
-      });
-    },
+            SurveyJS.JsonObject.metaData.addProperty("text", {
+                name: "progressBar",
+                type: "boolean",
+                category: "slider",
+                default: false,
+            });
+        },
 
-    afterRender: (question, el) => {
-      console.log("question", question);
-      el.classList.add("sliderContainer");
-      const dl = document.createElement("datalist");
-      dl.id = `${question.name}_slider_vals`;
-      question.valueLabels.forEach((label) => {
-        // TODO: labels will be spaced evenly, so need to "fill in the gaps" in the passed in values.
-        // This means we'll need to add ticks in some places, but not others, or stop spacing labels evenly.
-        //
-        var option = document.createElement("option");
-        option.value = label.value;
-        option.classList.add("sliderLabel");
-        console.log(label);
-        if (label.text) option.label = label.text;
-        dl.appendChild(option);
-      });
-      dl.classList.add("sliderLabels");
-      el.appendChild(dl);
+        afterRender: (question, el) => {
+            console.log("question", question);
+            el.classList.add("sliderContainer");
+            const dl = document.createElement("datalist");
+            dl.id = `${question.name}_slider_vals`;
+            question.valueLabels.forEach((label) => {
+                // TODO: labels will be spaced evenly, so need to "fill in the gaps" in the passed in values.
+                // This means we'll need to add ticks in some places, but not others, or stop spacing labels evenly.
+                //
+                // changes added for issue https://github.com/Watts-Lab/surveys/issues/110
+                var option = document.createElement("div");
+                option.classList.add("sliderLabel");
+                if (label.text) {
+                    option.innerHTML = `
+                        <div class="each_label">
+                            <div class="tick"></div>
+                            <div class="label_value">${label.text}</div>
+                        </div>
+                    `
+                    option.setAttribute("value" , label.value)
+                    dl.appendChild(option);
+                }
+            });
+                // changes added for issue https://github.com/Watts-Lab/surveys/issues/110            
+            dl.classList.add("sliderLabels");
+            el.appendChild(dl);
 
-      const slider = el.getElementsByTagName("input")[0];
-      slider.setAttribute("list", dl.id);
-      slider.classList.add("slider");
+            const slider = el.getElementsByTagName("input")[0];
+            slider.setAttribute("list", dl.id);
+            slider.classList.add("slider");
 
-      if (question.showProgressBar) slider.classList.add("progressBar");
+            if (question.showProgressBar) slider.classList.add("progressBar");
 
-      slider.addEventListener("mousedown", () =>
-        slider.classList.add("sliderClicked")
-      );
-    },
-  };
+            slider.addEventListener("mousedown", () =>
+                slider.classList.add("sliderClicked")
+            );
 
-  SurveyJS.CustomWidgetCollection.Instance.addCustomWidget(
-    widget,
-    "labeledRange"
-  );
+            // added additionally for highlighting label
+            let last = null
+            slider.addEventListener("input", e => {
+                let val = e.target.value
+                last?.classList.remove("active")
+                const activeEl = dl.querySelector(`[value='${val}']`)
+                activeEl?.classList.add("active")
+                last = activeEl
+            })
+        },
+    };
+
+    SurveyJS.CustomWidgetCollection.Instance.addCustomWidget(
+        widget,
+        "labeledRange"
+    );
 }
