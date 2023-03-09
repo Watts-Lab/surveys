fb6e57b62d600858412307e06f120ff7791c9c15
James Houghton <james.p.houghton@gmail.com>
2023-03-09 14:13:57 -0500

    update tests

--- surveys/partisanFeelingThermometerUS/partisanFeelingThermometerUS.cy.jsx
+++ surveys/partisanFeelingThermometerUS/partisanFeelingThermometerUS.cy.jsx
@@ -10,63 +10,34 @@ describe("PartisanFeelingThermometerUS", () => {
     cy.spy(dummy, "set").as("callback");
     cy.mount(<PartisanFeelingThermometerUS onComplete={dummy.set} />);
 
-    // cy.get('[data-name="tryToUnderstand"] input[value="3"]').click({
-    //   force: true,
-    // });
+    cy.get("input[type=range]")
+      .invoke("val", 25)
+      .trigger("mousedown", "center");
 
-    // cy.get('[data-name="askedQuestions"] input[value="3"]').click({
-    //   force: true,
-    // });
+    cy.get("form") // submit surveyJS form
+      .then(($form) => {
+        cy.wrap($form.find('input[type="button"][value="Next"]')).click();
+      });
 
-    // cy.get('[data-name="encouragedClarification"] input[value="3"]').click({
-    //   force: true,
-    // });
+    cy.get("input[type=range]")
+      .invoke("val", 25)
+      .trigger("mousedown", "center");
 
-    // cy.get('[data-name="expressedInterest"] input[value="3"]').click({
-    //   force: true,
-    // });
+    cy.get("form") // submit surveyJS form
+      .then(($form) => {
+        cy.wrap($form.find('input[type="button"][value="Complete"]')).click();
+      });
 
-    // Leave blank on purpose to check scoring algorithm
-    // cy.get('[data-name="listenedAttentively"] input[value="0"]').click({
-    //   force: true,
-    // });
-
-    // cy.get('[data-name="paidAttention"] input[value="0"]').click({
-    //   force: true,
-    // });
-
-    // cy.get('[data-name="gaveSpace"] input[value="0"]').click({ force: true });
-
-    // cy.get('[data-name="undividedAttention"] input[value="0"]').click({
-    //   force: true,
-    // });
-
-    // cy.get('[data-name="positiveAtmosphere"] input[value="3"]').click({
-    //   force: true,
-    // });
-
-    // cy.get('[data-name="allowedExpression"] input[value="3"]').click({
-    //   force: true,
-    // });
-
-    // cy.screenshot("constructiveListeningBehaviorsSelf/screenshot", {
-    //   overwrite: true,
-    // });
-
-    // cy.get("form") // submit surveyJS form
-    //   .then(($form) => {
-    //     cy.wrap($form.find('input[type="button"][value="Complete"]')).click();
-    //   });
-
+    // TODO: fix these assertions
     // cy.get("@callback").should("have.been.called");
     // cy.get("@callback").then((spy) => {
     //   const spyCall = spy.getCall(-1).args[0];
     //   console.log(spyCall);
-    //   expect(spyCall["result"]["rawScore"]).to.eq(3);
-    //   expect(spyCall["result"]["normScore"]).to.eq(1);
-    //   expect(spyCall["result"]["completion"]).to.eq(0.6);
-    //   expect(spyCall["responses"]["allowedExpression"]).to.eq(3);
-    //   expect(spyCall["responses"]["undividedAttention"]).to.be.undefined;
+    //   // expect(spyCall["result"]["rawScore"]).to.eq(3);
+    //   // expect(spyCall["result"]["normScore"]).to.eq(1);
+    //   // expect(spyCall["result"]["completion"]).to.eq(0.6);
+    //   expect(spyCall["responses"]["republicanTemp"]).to.eq(25);
+    //   expect(spyCall["responses"]["democratTemp"]).to.eq(25);
     // });
   });
 });
