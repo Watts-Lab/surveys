0736deedcfe509c473f94a37d6af7c75c39e3bdf
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2023-05-27 14:16:57 +0800

    record order + testing experiment

--- surveys/autonomyNeedSatisfaction/autonomyNeedSatisfaction.cy.jsx
+++ surveys/autonomyNeedSatisfaction/autonomyNeedSatisfaction.cy.jsx
@@ -6,10 +6,48 @@ const dummy = {
   set(response) {},
 };
 
+const keyWords = ["direction", "content", "choice", "important", 
+              "sensitive", "free", "voiced", "pressured"];
+
 describe("AutonomyNeedSatisfaction", () => {
   it("completes", () => {
     cy.spy(dummy, "set").as("callback");
     cy.mount(<AutonomyNeedSatisfaction onComplete={dummy.set} />);
+
+    //cy.get(".sv-row.sv-clearfix").click();
+/*    var index = 0;
+    var flag = false;
+    cy.get("#sp_101_content").children().each(($el) => {
+      if ($el.children().not.is(keyWords[index])) {
+        return true;
+      }
+    //  expect($el.children()).to.not.contain(keyWords[index])
+    //  cy.get($el).children().should("not.contain", keyWords[index])
+      index ++;
+    }); */
+    var same = false;
+    expect((same) => {
+      cy.get("#sp_101_content").children().each(($el) => {
+        if ($el.children().not.is(keyWords[index])) {
+          return false;
+        }
+        index ++;
+      });
+      return true;
+    }).equal(true);
+
+ /*   var i = 0;
+    Cypress.Commands.add("checkRandom", (panel) => {
+      cy.get(panel).children().each(($el) => {
+        if($el.children().not.contain(keyWords[i])) {
+          return true;
+        } 
+        i++;
+      })
+      return false;
+    })
+
+    cy.checkRandom("#sp_101_content").should("be.equal", "false"); */
 
     cy.get('[data-name="chooseDirection"] input[value="2"]').click({
       force: true,
