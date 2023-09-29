0159b8a3789620395b7f0cd69bbbc7f5672792f1
Xinlan Emily Hu <xehu@stanford.edu>
2023-09-29 11:09:16 -0400

    add SVI survey folder -- somehow was not pushed to github earlier

---
+++ surveys/SVI/README.md
@@ -0,0 +1,22 @@
+# Subjective Value Inventory
+
+## Survey Purpose
+
+The purpose of this survey is to measure the range of social psychological outcomes of negotiation that are valued *subjectively*, rather than objectively (e.g., monetary pay-off). This construct, *subjective value*, is a key predictor of future negotiation decisions.
+
+## Theoretical construct
+
+Per the original paper by Curhan et al. (2006), one's subjective feelings about negotiation involves 4 underlying subconstructs: (1) Feelings About the Instrumental Outcome, (2) Feelings About the Self, (3) Feelings About the Negotiation Process, and (4) Feelings About the Relationship.
+
+# Adaptations
+
+We stuck to the original survey from http://www.subjectivevalue.com/ as closely as possible. The only modification was that we shortened the lengths of some of the scale points.
+
+- "It positively/negatively impacted my self-image" was modified to "Extremely Negative/Neutral/Extremely Positive"
+- "It made me feel more/less competent" was modified to "Much Less/Neutral/Much More"
+
+The "NA" option in the original was retained and encoded as undefined, such that all NA responses are dropped from sub-scales when calculating the result.
+
+### Screenshot
+
+![Screenshot](screenshot.png)
---
+++ surveys/SVI/SVI.score.js
@@ -0,0 +1,97 @@
+function reverseCode(value, minVal, maxVal) {
+  const floatVal = parseFloat(value);
+  if (Number.isNaN(floatVal)) return undefined;
+  return maxVal - (value - minVal);
+}
+
+function sum(array) {
+  return array.reduce((acc, val) => acc + val, 0);
+}
+
+function mean(array) {
+  //Ensure Mean is robust to NA's
+  const filteredArr = array.filter(num => typeof num === 'number' && !isNaN(num));
+  if (filteredArr.length === 0) {
+    return NaN;
+  }
+  return sum(array) / filteredArr.length;}
+
+export default function scoreFunc(responses) {
+  const minVal = 1;
+  const maxVal = 7;
+
+  // Instrumental Outcome
+  const rawInstrumentalOutcomeValues = [
+    responses["Instrumental Outcome 1"],
+    responses["Instrumental Outcome 2"],
+    responses["Instrumental Outcome 4"],
+    reverseCode(responses["Instrumental Ouctome 3"], minVal, maxVal)
+  ].map(parseFloat);
+
+  // Self
+  const rawSelfValues = [
+    reverseCode(responses["Self 1"], minVal, maxVal),
+    responses["Self 2"],
+    responses["Self 3"],
+    responses["Self 4"]
+  ].map(parseFloat);
+
+  // Process
+  const rawProcessValues = [
+    responses["Process 1"],
+    responses["Process 2"],
+    responses["Process 3"],
+    responses["Process 4"]
+  ].map(parseFloat);
+
+  // Relationship
+  const rawRelationshipValues = [
+    responses["Relationship 1"],
+    responses["Relationship 2"],
+    responses["Relationship 3"],
+    responses["Relationship 4"]
+  ].map(parseFloat);
+
+  // Global
+  const rawGlobalValues = [
+    responses["Instrumental Outcome 1"],
+    responses["Instrumental Outcome 2"],
+    responses["Instrumental Outcome 4"],
+    reverseCode(responses["Instrumental Ouctome 3"], minVal, maxVal),
+    reverseCode(responses["Self 1"], minVal, maxVal),
+    responses["Self 2"],
+    responses["Self 3"],
+    responses["Self 4"],
+    responses["Process 1"],
+    responses["Process 2"],
+    responses["Process 3"],
+    responses["Process 4"],
+    responses["Relationship 1"],
+    responses["Relationship 2"],
+    responses["Relationship 3"],
+    responses["Relationship 4"]
+  ].map(parseFloat);
+
+  // Rapport
+  const rawRapportValues = [
+    responses["Process 1"],
+    responses["Process 2"],
+    responses["Process 3"],
+    responses["Process 4"],
+    responses["Relationship 1"],
+    responses["Relationship 2"],
+    responses["Relationship 3"],
+    responses["Relationship 4"]
+  ].map(parseFloat);
+
+
+  const result = {
+    InstrumentalOutcome: mean(rawInstrumentalOutcomeValues).toFixed(3),
+    Self: mean(rawSelfValues).toFixed(3),
+    Process: mean(rawProcessValues).toFixed(3),
+    Relationship: mean(rawRelationshipValues).toFixed(3),
+    Global: mean(rawGlobalValues).toFixed(3),
+    Rapport: mean(rawRapportValues).toFixed(3)};
+    
+  return result;
+}
