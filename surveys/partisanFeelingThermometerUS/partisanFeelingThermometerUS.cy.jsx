1d2bf33161e95f4e3dcac1b1ef5a65ca2dbcc52a
James Houghton <james.p.houghton@gmail.com>
2023-11-16 10:34:38 -0500

    update cypress and debug

--- surveys/partisanFeelingThermometerUS/partisanFeelingThermometerUS.cy.jsx
+++ surveys/partisanFeelingThermometerUS/partisanFeelingThermometerUS.cy.jsx
@@ -8,39 +8,31 @@ const dummy = {
 describe("PartisanFeelingThermometerUS", () => {
   it("completes", () => {
     cy.spy(dummy, "set").as("callback");
-    cy.mount(<PartisanFeelingThermometerUS onComplete={dummy.set} />);
     cy.viewport("macbook-11");
+    cy.mount(<PartisanFeelingThermometerUS onComplete={dummy.set} />);
 
-    // check randomization by reloading until it gets a democrat prompt first
-    if (
-      cy
-        .get("#sq_102_ariaTitle > .sv-string-viewer")
-        .invoke("text")
-        .then((text) => {
-          if (text === "How would you rate Republicans?") {
-            cy.reload();
-            cy.mount(<PartisanFeelingThermometerUS onComplete={dummy.set} />);
-          }
-        })
-    )
-      cy.get("input[type=range]").invoke("val", 75).click({ force: true });
-
-    cy.screenshot("lonelinessSingleItem/screenshot_page1", { overwrite: true });
-
-    cy.get("form") // submit surveyJS form
-      .then(($form) => {
-        cy.wrap($form.find('input[type="button"][value="Next"]')).click({
-          force: true,
+    cy.get("body")
+      .then(($body) => {
+        if ($body.find(`div[data-name="republicanTemp"]`).length) {
+          console.log("Reloading to get democrat prompt first");
+          cy.reload();
+          cy.mount(<PartisanFeelingThermometerUS onComplete={dummy.set} />);
+        }
+      })
+      .then(() => {
+        cy.contains("How would you rate Democrats");
+        cy.get("input[type=range]").invoke("val", 75).click({ force: true });
+        cy.screenshot("lonelinessSingleItem/screenshot_page1", {
+          overwrite: true,
         });
-      });
+        cy.get(`input[type="button"][value="Next"]`).click({ force: true });
 
-    cy.get("input[type=range]").invoke("val", 25).click({ force: true });
-
-    cy.screenshot("lonelinessSingleItem/screenshot_page2", { overwrite: true });
-
-    cy.get("form") // submit surveyJS form
-      .then(($form) => {
-        cy.wrap($form.find('input[type="button"][value="Complete"]')).click();
+        cy.contains("How would you rate Republicans");
+        cy.get("input[type=range]").invoke("val", 25).click({ force: true });
+        cy.screenshot("lonelinessSingleItem/screenshot_page2", {
+          overwrite: true,
+        });
+        cy.get(`input[type="button"][value="Complete"]`).click({ force: true });
       });
 
     cy.get(".sv-body").should("not.exist");
@@ -54,17 +46,5 @@ describe("PartisanFeelingThermometerUS", () => {
       expect(spyCall.result.rawScore).to.eq(-0.5);
       expect(spyCall.result.normScore).to.eq(0.5);
     });
-
-    // TODO: fix these assertions
-    // cy.get("@callback").should("have.been.called");
-    // cy.get("@callback").then((spy) => {
-    //   const spyCall = spy.getCall(-1).args[0];
-    //   console.log(spyCall);
-    //   // expect(spyCall["result"]["rawScore"]).to.eq(3);
-    //   // expect(spyCall["result"]["normScore"]).to.eq(1);
-    //   // expect(spyCall["result"]["completion"]).to.eq(0.6);
-    //   expect(spyCall["responses"]["republicanTemp"]).to.eq(25);
-    //   expect(spyCall["responses"]["democratTemp"]).to.eq(25);
-    // });
   });
 });
