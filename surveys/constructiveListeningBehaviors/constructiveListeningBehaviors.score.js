9f406fdfb5a6ea76587a5f2c35091e3811d84236
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2024-06-14 09:19:33 -0400

    Millie check randomization custom command (#140)
    * question order actually randomized
    
    With "questionsOnPageMode": "singlePage" originally, question order are not randomized for some reason. #108
    
    * record order + testing experiment
    
    * question order recorded + experiment with testing
    
    * revised recording order
    
    * test randomization for autonomyNeedSatisfaction
    
    * test randomization
    
    * test randomization
    
    * partisonFeelingThermo cypress + scoring
    
    * new scoring method
    
    result contains normalized republican & democrat score and the norm score (absolute value of the 2)
    
    * thermometer randomization test
    
    * modify scoring
    
    raw score positive means favorable to republican, and vice versa
    
    * Check for questions by name, check that all are present
    
    * custom command
    
    * custom command #137
    
    added custom command that checks randomization of order
    
    * added comments on partisonFeelingThermo
    
    * add randomization checker to new surveys
    
    * debugging
    
    * update dependencies and bump version
    
    * update cypress and debug
    
    * update build
    
    * Update package-lock.json
    
    * Update package.json
    
    * Update package.json
    
    * update perceptionOfOthers test
    
    ---------
    
    Co-authored-by: James Houghton <james.p.houghton@gmail.com>

--- surveys/constructiveListeningBehaviors/constructiveListeningBehaviors.score.js
+++ surveys/constructiveListeningBehaviors/constructiveListeningBehaviors.score.js
@@ -31,10 +31,13 @@ export default function scoreFunc(responses) {
 
   const normedValues = normalize(completedValues, minVal, maxVal);
 
+  const order = Object.keys(responses);
+
   const result = {
     rawScore: mean(completedValues).toFixed(3),
     normScore: mean(normedValues).toFixed(3),
     completion: completedValues.length / rawValues.length,
+    order: order,
   };
   return result;
 }
--- surveys/partisanFeelingThermometerUS/partisanFeelingThermometerUS.cy.jsx
+++ surveys/partisanFeelingThermometerUS/partisanFeelingThermometerUS.cy.jsx
@@ -8,39 +8,43 @@ const dummy = {
 describe("PartisanFeelingThermometerUS", () => {
   it("completes", () => {
     cy.spy(dummy, "set").as("callback");
+    cy.viewport("macbook-11");
     cy.mount(<PartisanFeelingThermometerUS onComplete={dummy.set} />);
-    cy.viewport('macbook-11');
 
-    cy.get("input[type=range]")
-      .invoke("val", 25)
-      .trigger("mousedown", "center");
+    cy.get("body")
+      .then(($body) => {
+        if ($body.find(`div[data-name="republicanTemp"]`).length) {
+          console.log("Reloading to get democrat prompt first");
+          cy.reload();
+          cy.mount(<PartisanFeelingThermometerUS onComplete={dummy.set} />);
+        }
+      })
+      .then(() => {
+        cy.contains("How would you rate Democrats");
+        cy.get("input[type=range]").invoke("val", 75).click({ force: true });
+        cy.screenshot("lonelinessSingleItem/screenshot_page1", {
+          overwrite: true,
+        });
+        cy.get(`input[type="button"][value="Next"]`).click({ force: true });
 
-    cy.get("form") // submit surveyJS form
-      .then(($form) => {
-        cy.wrap($form.find('input[type="button"][value="Next"]')).click();
-      });
-
-    cy.get("input[type=range]")
-      .invoke("val", 25)
-      .trigger("mousedown", "center");
-
-    cy.get("form") // submit surveyJS form
-      .then(($form) => {
-        cy.wrap($form.find('input[type="button"][value="Complete"]')).click();
+        cy.contains("How would you rate Republicans");
+        cy.get("input[type=range]").invoke("val", 25).click({ force: true });
+        cy.screenshot("lonelinessSingleItem/screenshot_page2", {
+          overwrite: true,
+        });
+        cy.get(`input[type="button"][value="Complete"]`).click({ force: true });
       });
 
     cy.get(".sv-body").should("not.exist");
-    
-    // TODO: fix these assertions
-    // cy.get("@callback").should("have.been.called");
-    // cy.get("@callback").then((spy) => {
-    //   const spyCall = spy.getCall(-1).args[0];
-    //   console.log(spyCall);
-    //   // expect(spyCall["result"]["rawScore"]).to.eq(3);
-    //   // expect(spyCall["result"]["normScore"]).to.eq(1);
-    //   // expect(spyCall["result"]["completion"]).to.eq(0.6);
-    //   expect(spyCall["responses"]["republicanTemp"]).to.eq(25);
-    //   expect(spyCall["responses"]["democratTemp"]).to.eq(25);
-    // });
+
+    cy.get("@callback").should("have.been.called");
+    cy.get("@callback").then((spy) => {
+      const spyCall = spy.getCall(-1).args[0];
+      console.log(spyCall);
+      expect(spyCall.result.normRepublicanTemp).to.eq(0.25);
+      expect(spyCall.result.normDemocratTemp).to.eq(0.75);
+      expect(spyCall.result.rawScore).to.eq(-0.5);
+      expect(spyCall.result.normScore).to.eq(0.5);
+    });
   });
 });
