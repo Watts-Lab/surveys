2f6997b4018163bf77b5bdbfbf5ef538eef212cd
amirrr <6696894+amirrr@users.noreply.github.com>
2024-10-04 21:53:46 +0200

    Add failing test

--- surveys/CRT/CRT.cy.jsx
+++ surveys/CRT/CRT.cy.jsx
@@ -8,7 +8,7 @@ const dummy = {
 };
 
 describe("CRT", () => {
-  it("completes the survey", () => {
+  it("fail if not entered correctly", () => {
     cy.spy(dummy, "set").as("callback");
     cy.mount(<CRT onComplete={dummy.set} />);
     cy.viewport("macbook-11");
@@ -18,6 +18,28 @@ describe("CRT", () => {
     cy.get('[data-name="toaster"] input[type="number"]').type(125); // correct
     cy.get('[data-name="apples"] input[type="number"]').type(-3); // wrong
     cy.get('[data-name="eggs"] input[type="number"]').type("cat"); // wrong
+    cy.get('[data-name="dog_cat"] input[type="number"]').type(100000000000); // wrong
+
+    cy.get("form") // submit surveyJS form
+      .then(($form) => {
+        cy.wrap($form.find('input[type="button"][value="Complete"]')).click();
+      });
+
+    cy.get(".sv-body").should("exist");
+
+    cy.get("@callback").should("not.have.been.called");
+  });
+
+  it("completes the survey", () => {
+    cy.spy(dummy, "set").as("callback");
+    cy.mount(<CRT onComplete={dummy.set} />);
+    cy.viewport("macbook-11");
+
+    cy.get('[data-name="drill_hammer"] input[type="number"]').type(15); // correct
+    cy.get('[data-name="rachel"] input[type="number"]').type(19); // correct
+    cy.get('[data-name="toaster"] input[type="number"]').type(125); // correct
+    cy.get('[data-name="apples"] input[type="number"]').type(-3); // wrong
+    cy.get('[data-name="eggs"] input[type="number"]').type(10); // wrong
     cy.get('[data-name="dog_cat"] input[type="number"]').type(100000000000); // wrong
 
     cy.screenshot("CRT/screenshot", {
