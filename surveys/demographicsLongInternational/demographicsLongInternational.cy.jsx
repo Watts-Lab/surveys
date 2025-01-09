1dd5a3c97704d673df75ec3331797be42df7ca7e
amirrr <6696894+amirrr@users.noreply.github.com>
2025-01-09 19:23:46 +0100

    refactor demographics tests to utilize dynamic country data and improve structure

--- surveys/demographicsLongInternational/demographicsLongInternational.cy.jsx
+++ surveys/demographicsLongInternational/demographicsLongInternational.cy.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { DemographicsLongInternational } from "../../src/index";
+import { getContainerEl } from "cypress/react";
+import ReactDom from "react-dom";
 
 const dummy = {
   set(response) {},
 };
+
+import demographicsData from "./demographicsLongInternational.json";
 
 describe("Demographics", () => {
   it("completes", () => {
@@ -47,7 +51,9 @@ describe("Demographics", () => {
     cy.get(`[data-name="country_reside"] input`).click({ force: true });
     cy.contains("United States").click({ force: true });
 
-    cy.screenshot("./demographicsLongInternational/screenshotGeneral", { overwrite: true });
+    cy.screenshot("./demographicsLongInternational/screenshotGeneral", {
+      overwrite: true,
+    });
 
     cy.get(`input[type="button"][value="Next"]`).click({ force: true });
 
@@ -59,9 +65,7 @@ describe("Demographics", () => {
       .next()
       .click({ force: true });
 
-    cy.get(`[data-name="zipcode_US"] input`).click().type(
-      "52066"
-    );
+    cy.get(`[data-name="zipcode_US"] input`).click().type("52066");
 
     cy.get(`[data-name="race_US"] input[value="White"]`)
       .next()
@@ -74,7 +78,9 @@ describe("Demographics", () => {
       force: true,
     });
 
-    cy.screenshot("./demographicsLongInternational/screenshotUSA", { overwrite: true });
+    cy.screenshot("./demographicsLongInternational/screenshotUSA", {
+      overwrite: true,
+    });
 
     cy.get(`input[type="button"][value="Complete"]`).click({ force: true });
 
@@ -89,69 +95,109 @@ describe("Demographics", () => {
     });
   });
 
-  it("Completes Other Country", () => {
+  it("Completes Other Countries", () => {
+    const country_list = demographicsData.pages[0].elements
+      .find((element) => element.name === "country_reside")
+      .choices.map((choice) => choice.value);
+
+    const countrySalaries = {};
+    for (const country of country_list) {
+      const country_code = country
+        .toLowerCase()
+        .replace(new RegExp(" ", "g"), "_")
+        .replace(",", "_")
+        .replace("(", "_")
+        .replace(")", "_");
+      let salaryFound = false;
+      for (let i = 1; i < demographicsData.pages.length; i++) {
+        const page = demographicsData.pages[i];
+        const incomeElement = page.elements.find(
+          (element) => element.name === `income_${country_code}`
+        );
+        if (incomeElement) {
+          const salary = incomeElement.choices[0];
+          countrySalaries[country_code] = salary;
+          salaryFound = true;
+          break;
+        }
+      }
+      if (!salaryFound) {
+        console.warn(`Salary data not found for country: ${country_code}`);
+      }
+    }
+
     cy.spy(dummy, "set").as("callback");
-    cy.mount(<DemographicsLongInternational onComplete={dummy.set} />);
-    cy.viewport("macbook-11");
 
-    cy.get(`[data-name="birth_year"] input`).click().type("1985");
+    country_list.slice(0, 10).forEach((country) => {
+      const country_code = country
+        .toLowerCase()
+        .replace(new RegExp(" ", "g"), "_")
+        .replace(",", "_")
+        .replace("(", "_")
+        .replace(")", "_");
+      console.log(country, country_code);
 
-    cy.get(`[data-name="gender"] input[value="other"]`).click({ force: true });
+      cy.mount(
+        <DemographicsLongInternational
+          storageName={country_code}
+          onComplete={dummy.set}
+        />
+      );
 
-    cy.get(`[data-name="gender_other"] input`).click().type("Other gender");
+      // Proceed with filling out the survey
+      cy.viewport("macbook-11");
+      cy.get(`[data-name="birth_year"] input`).click().type("1985");
+      cy.get(`[data-name="gender"] input[value="other"]`).click({
+        force: true,
+      });
+      cy.get(`[data-name="gender_other"] input`).click().type("Other gender");
+      cy.get(`[data-name="marital_status"] input`).click({ force: true });
+      cy.contains("Married or Domestic Partnership").click({ force: true });
+      cy.get(`[data-name="language_primary"] input`).click({ force: true });
+      cy.contains("French").click({ force: true });
+      cy.get(`[data-name="english_written"] input[value="4"]`).click({
+        force: true,
+      });
+      cy.get(`[data-name="english_spoken"] input[value="4"]`).click({
+        force: true,
+      });
+      cy.get(`[data-name="employment_status"] input[value="employed"]`).click({
+        force: true,
+      });
 
-    cy.get(`[data-name="marital_status"] input`).click({ force: true });
+      cy.get(`[data-name="country_reside"] input`).click({ force: true });
+      cy.get(`[data-name="country_reside"]`)
+        .contains(country)
+        .click({ force: true });
 
-    cy.contains("Married or Domestic Partnership").click({ force: true });
+      cy.get(`input[type="button"][value="Next"]`).click({ force: true });
+      cy.get(
+        `[data-name="education_${country_code}"] input[value="Other (please specify)"]`
+      ).click({
+        force: true,
+      });
+      cy.get(`[data-name="other_education_${country_code}"] input`)
+        .click()
+        .type("Survey Developer");
+      cy.get(`[data-name="zipcode_${country_code}"] input`)
+        .click()
+        .type("82919123");
+      cy.get(
+        `[data-name="ethnicity_${country_code}"] input[value="Middle Eastern or North African"]`
+      )
+        .next()
+        .click({ force: true });
+      cy.get(
+        `[data-name="income_${country_code}"] input[value="${countrySalaries[country_code]}"]`
+      ).click({
+        force: true,
+      });
+      cy.get(`input[type="button"][value="Complete"]`).click({ force: true });
 
-    cy.get(`[data-name="language_primary"] input`).click({ force: true });
-    cy.contains("French").click({ force: true });
-
-    cy.get(`[data-name="english_written"] input[value="4"]`).click({
-      force: true,
+      cy.wait(100);
     });
-
-    cy.get(`[data-name="english_spoken"] input[value="4"]`).click({
-      force: true,
-    });
-
-    cy.get(`[data-name="employment_status"] input[value="employed"]`).click({
-      force: true,
-    });
-
-    cy.get(`[data-name="country_reside"] input`).click({ force: true });
-    cy.contains("Iran").click({ force: true });
-
-    cy.get(`input[type="button"][value="Next"]`).click({ force: true });
-
-    cy.get(`[data-name="education_iran"] input[value="Other (please specify)"]`).click({
-      force: true,
-    });
-
-    cy.get(`[data-name="other_education_iran"] input`).click().type("Survey Developer");
-
-    cy.get(`[data-name="zipcode_iran"] input`).click().type("82919123");
-
-    cy.get(
-      `[data-name="ethnicity_iran"] input[value="Middle Eastern or North African"]`
-    )
-      .next()
-      .click({ force: true });
-
-    cy.get(
-      `[data-name="income_iran"] input[value="Less than 135,600,000"]`
-    ).click({
-      force: true,
-    });
-
-    cy.screenshot("./demographicsLongInternational/screenshotIran", {
-      overwrite: true,
-    });
-
-    cy.get(`input[type="button"][value="Complete"]`).click({ force: true });
 
     cy.get(".sv-body").should("not.exist");
-
     cy.get("@callback").should("have.been.called");
     cy.get("@callback").then((spy) => {
       const spyCall = spy.getCall(-1).args[0];
